337b70e9be90b39860985a3c4718138d35eab81e
Corvin Gröning <114770097+cgroening@users.noreply.github.com>
2025-05-28 11:29:47 +0200

    Refactor logging configuration: remove commented-out code and clean up imports

--- src/main.py
+++ src/main.py
@@ -2,14 +2,6 @@ import logging
 from app import TuidoApp
 
 
-# logging_levels = [logging.WARNING, logging.DEBUG, logging.INFO,
-#                   logging.CRITICAL, logging.ERROR]
-
-# for logging_level in logging_levels:
-#     logging.basicConfig(filename="info.log", filemode="w", level=logging_level,
-#                         format="%(asctime)s [%(levelname)s] %(message)s")
-# _ = logging.getLogger(__name__)
-
 logging.basicConfig(
     filename='log/info.log',
     filemode='w',
